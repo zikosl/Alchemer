ae6a312b518f6d299c38c31482db56de1cc05757
thunderbird <thunderbird08280@gmail.com>
2025-02-18 07:49:46 -0800

    public project

---
+++ src/components/DetailPage.jsx
@@ -0,0 +1,14 @@
+// DetailPage.js
+import React from "react";
+import Topbar from "./TopBar";
+
+const DetailPage = () => {
+  return (
+    <div>
+      <Topbar />
+      DetailPage Component
+    </div>
+  );
+};
+
+export default DetailPage;
